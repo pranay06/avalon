920605f1d41a6f3819a8c7c53957eb13058f1ce2
divya <divya.taori@wipro.com>
2020-02-10 12:41:39 +0000

    Revert "Add Apachec License details and fix typos"
    This reverts commit b96a0c1daba632262631bb8979d1a6e890d55a16.
    
    Signed-off-by: divya <divya.taori@wipro.com>

--- client_sdk/avalon_client_sdk/quorum/contracts/WorkOrder.sol
+++ client_sdk/avalon_client_sdk/quorum/contracts/WorkOrder.sol
@@ -66,7 +66,7 @@ contract WorkOrder {
 
     //-------------------------Function definitions----------------------------
 
-    //Restrict call from authorised requester
+    //Retrict call from authorised requester
     function onlyRequester(address requester) internal view returns (bool) {
         if (msg.sender == requester) {
             return true;
